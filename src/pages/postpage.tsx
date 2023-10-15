6e4a489eb5a3388445ca7e9271e53c1a5aad267c
promptaction8 <jinwoo30754@naver.com>
2023-10-15 17:13:35 +0900

    name change

--- src/pages/postpage.tsx
+++ src/pages/postpage.tsx
@@ -1,42 +1,15 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { Provider } from 'jotai'
+import Post from '@/components/post'
+import PostInput from '@/components/postInput'
 
-function PostForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm()
-
-    const onSubmit = (data) => {
-        // 제출된 데이터를 처리하는 로직을 작성
-        console.log(data)
-    }
-
+const App = () => {
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="title">제목</label>
-                <input
-                    type="text"
-                    id="title"
-                    {...register('title', { required: true })}
-                />
-                {errors.title && <span>제목을 입력해주세요.</span>}
-            </div>
-            <div>
-                <label htmlFor="content">내용</label>
-                <textarea
-                    id="content"
-                    {...register('content', { required: true })}
-                />
-                {errors.content && <span>내용을 입력해주세요.</span>}
-            </div>
-            <div>
-                <button type="submit">게시</button>
-            </div>
-        </form>
+        <Provider>
+            <Post />
+            <PostInput />
+        </Provider>
     )
 }
 
-export default PostForm
+export default App
